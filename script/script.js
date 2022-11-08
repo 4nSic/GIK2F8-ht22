8210c7b10360c58ee2041d1dfbf5b94705233e0a
4nSic <daniel.a.dalberg@gmail.com>
2022-11-08 14:32:32 +0100

    Jobbat med javascript vs DOM, ej klart

--- script/script.js
+++ script/script.js
@@ -18,7 +18,6 @@ const bookList = [
     }
 ];
 
-
 function handelKeyPress(e){
     /* 
         Ska taemot och läsa av värdet i i nputfältet.
@@ -50,11 +49,32 @@ function searchBokks(searchTerm){
     return filterdList;
 }
 
-
-
 function renderBookList(bookList){
+    if (bookList.length >0) {
 
+        let html = `<ul class="book-list rounded-md border-2 border-blue-400 bg-with w-full mx-auto">`;
+        for (let index = 0; index < bookList.length; index++) {
+            html += `<li class="book-list__item mx-2 mb-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b border-indigo-700 cursor-pointer">
+                    ${bookList[index].author} - ${bookList[index].title}       
+                    </li>`
     
+            
+        }
+        html += `</ul>`;
+        
+        const existingElement = document.querySelector(".book-list");
+        const root = document.getElementById("root");
+        if (existingElement) {
+            existingElement.replaceChildren();
+        }
+        
+        const htmlElement = document.createElement("ul");
+    
+        root.insertAdjacentHTML("beforeend",html);
+    }
+
+
+
 
 }
 
