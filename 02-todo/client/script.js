c7878c598314378054c8d60cc325cec974a3c6fb
Daniel Dalberg <daniel.a.dalberg@gmail.com>
2022-12-08 09:38:42 +0100

    validering

--- 02-todo/client/script.js
+++ 02-todo/client/script.js
@@ -7,8 +7,54 @@ todoForm.dueDate.addEventListener("blur",(e) => validateField(e.target));
 
 todoForm.addEventListener("submit", onSubmit);
 
-function validateField(e){
-    console.log(e);
+let titleValid = false;
+let descriptionValid = false;
+let dueDateValid = false;
+
+function validateField(field){
+    const {name, value} = field;
+    let validationMessage="";
+
+    switch(name){
+        case "title":{
+            if(value.length < 2){
+                validationMessage ="Fältet 'Title' måste innehålla minst två tcken";              
+            }
+            else if(value.length > 100){
+                validationMessage ="Fältet 'Title' måste innehålla mindre än 100 tcken";
+            }
+            else{
+                titleValid = true
+            }
+            
+            break;
+        }
+
+        case "description":{
+            if(value.length > 450){
+                validationMessage="Fältet 'Beskrvning' får inte innehålla mer än 500 tecken";
+            }
+            else{
+                descriptionValid = true;
+            }
+
+            break;
+        }
+
+        case "dueDate":{
+            if(value.length === 0 ){
+                validationMessage="Fältet 'Slutförd senast' är obligatorisk ";
+            }
+            else{
+                dueDateValid = true;
+            }
+
+            break;
+        }
+    }
+
+    field.previousElementSibling.innerText = validationMessage;
+    field.previousElementSibling.classList.remove("hidden")
 }
 
 function onSubmit(e){
